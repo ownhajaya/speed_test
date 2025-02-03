6895be2a2b3b3a562dc90fdc119d8b89bd0ac015
ownhajaya <hajayaown@gmail.com>
2025-02-03 15:46:19 +0300

    task 3/2/2025  3:46

--- speed_test/lib/main.dart
+++ speed_test/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'example/uiux_figma.dart';
 import 'screens/onboarding_screen.dart';
 
 // onboarding / login / home
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
         useMaterial3: true,
       ),
-      home: const OnboardingScreen(),
+      home: const UiuxFigma(),
     );
   }
 }
